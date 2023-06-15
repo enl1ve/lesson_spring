e3879af9c7f3891a608797f30a77dd5f076ac426
Sergey Baklanov <sersbakalnov@gmail.com>
2023-06-16 02:02:18 +0300

    l3 DI and solution

--- src/main/java/pract_1/TestSpring.java
+++ src/main/java/pract_1/TestSpring.java
@@ -8,11 +8,15 @@ public class TestSpring {
             "applicationContext.xml"
         );
 
-        Music music = context.getBean("musicBean", Music.class);
+        //Music music = context.getBean("musicBean", Music.class);
+        //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-
+        // Внедрение DI
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
+
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
