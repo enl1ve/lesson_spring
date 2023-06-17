b3253b5fec4758158ec3d23b6e493b614d30621a
Sergey Baklanov <sersbakalnov@gmail.com>
2023-06-17 21:56:37 +0300

    ls9 factory-method and lifecycle

--- src/main/java/pract_1/ClassicalMusic.java
+++ src/main/java/pract_1/ClassicalMusic.java
@@ -1,7 +1,22 @@
 package pract_1;
 
 public class ClassicalMusic implements Music{
+    private ClassicalMusic() {
+    }
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
     public String getSong() {
         return "Sonata 12";
+    }
+
+    public void doMyInit() {
+        System.out.println("My init");
+    }
+
+    public void destroyMyInit() {
+        System.out.println("Destroy");
     }
 }
--- src/main/java/pract_1/TestSpring.java
+++ src/main/java/pract_1/TestSpring.java
@@ -8,29 +8,35 @@ public class TestSpring {
             "applicationContext.xml"
         );
 
-        //Music music = context.getBean("musicBean", Music.class);
-        //MusicPlayer musicPlayer = new MusicPlayer(music);
+        ClassicalMusic classicalMusic = context.getBean("musicBean3", ClassicalMusic.class);
+        ClassicalMusic classicalMusic1 = context.getBean("musicBean3", ClassicalMusic.class);
 
-        // Внедрение DI
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println(classicalMusic.getSong()); //My Init -> bla-bla-bal -> Destroy
+        System.out.println(classicalMusic1.getSong()); //My Init -> bla-bla-bal -> Destroy
 
-        boolean con = musicPlayer1 == musicPlayer2;
-        System.out.println(con);
-
-        System.out.println(musicPlayer1);
-        System.out.println(musicPlayer2);
-
-        musicPlayer1.setVolume(15);
-
-        System.out.println(musicPlayer1.getVolume()); //15
-        System.out.println(musicPlayer2.getVolume()); // и тут 15 потому что сингтон метод.. После изменения скоупа вернулись 50 процентов
-                                                        // скоуп Прототип делает для каждого вызова новый бин
-
-        //musicPlayer.playMusic();
-
-//        System.out.println(musicPlayer.getName());
-//        System.out.println(musicPlayer.getVolume());
+//        //Music music = context.getBean("musicBean", Music.class);
+//        //MusicPlayer musicPlayer = new MusicPlayer(music);
+//
+//        // Внедрение DI
+//        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean con = musicPlayer1 == musicPlayer2;
+//        System.out.println(con);
+//
+//        System.out.println(musicPlayer1);
+//        System.out.println(musicPlayer2);
+//
+//        musicPlayer1.setVolume(15);
+//
+//        System.out.println(musicPlayer1.getVolume()); //15
+//        System.out.println(musicPlayer2.getVolume()); // и тут 15 потому что сингтон метод.. После изменения скоупа вернулись 50 процентов
+//                                                        // скоуп Прототип делает для каждого вызова новый бин
+//
+//        //musicPlayer.playMusic();
+//
+////        System.out.println(musicPlayer.getName());
+////        System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
