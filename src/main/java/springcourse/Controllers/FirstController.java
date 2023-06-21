20724fdc665569208fc01d8ee9a184b7a5147665
Sergey Baklanov <sersbakalnov@gmail.com>
2023-06-21 13:52:26 +0300

    ls21 Model

--- src/main/java/springcourse/Controllers/FirstController.java
+++ src/main/java/springcourse/Controllers/FirstController.java
@@ -1,6 +1,7 @@
 package springcourse.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,16 +14,42 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name, //RequestParam говорит о том что параметры обязательны
-                                                                                        // если не указыывать required=false
-                            @RequestParam(value = "surname", required = false) String surname ) {
+                            // если не указыывать required=false
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-        System.out.println("Hello, " + name + " " + surname);
-
+        //System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodbyePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("calculator")
+    public String calculator(@RequestParam("a") int a,
+                             @RequestParam("b") int b,
+                             @RequestParam("action") String action,
+
+                             Model model) {
+
+        switch (action){
+            case ("addition"):
+                model.addAttribute("action", "Result = " + (a+b));
+                break;
+            case("multiplication"):
+                model.addAttribute("action", "Result = " + (a*b));
+                break;
+            case ("subtraction"):
+                model.addAttribute("action", "Result = " + (a-b));
+                break;
+            case ("division"):
+                model.addAttribute("action", "Result = " + (a/b));
+                break;
+        }
+
+        return "first/calculator";
     }
 }
