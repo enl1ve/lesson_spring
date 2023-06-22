efbff5f05817fd480da7c79bfd7e7cc68e6f0c44
Sergey Baklanov <sersbakalnov@gmail.com>
2023-06-22 19:39:22 +0300

    ls23 @ModelAttribute

--- src/main/java/spring_course/controller/peopleController.java
+++ src/main/java/spring_course/controller/peopleController.java
@@ -3,11 +3,9 @@ package spring_course.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import spring_course.dao.PersonDao;
+import spring_course.model.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -31,7 +29,18 @@ public class peopleController {
     public String show(@PathVariable("id") int id,
                        Model model) {
         // получаем одного человека по его айди из ДАО и предаем этого человека на отображение
-        model.addAttribute("persone", personDao.show(id));
+        model.addAttribute("person", personDao.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDao.save(person);
+        return "redirect:/people";
     }
 }
--- src/main/java/spring_course/dao/PersonDao.java
+++ src/main/java/spring_course/dao/PersonDao.java
@@ -1,6 +1,8 @@
 package spring_course.dao;
 
 import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import spring_course.model.Person;
 
 import java.util.ArrayList;
@@ -27,5 +29,10 @@ public class PersonDao {
 
     public Person show(int id) {
         return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
     }
 }
