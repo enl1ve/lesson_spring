2e5585f06ff62cdcc852e2624dca2d7b635abaae
Sergey Baklanov <sersbakalnov@gmail.com>
2023-06-22 20:24:20 +0300

    ls24 complete CRUD app

--- src/main/java/spring_course/dao/PersonDao.java
+++ src/main/java/spring_course/dao/PersonDao.java
@@ -35,4 +35,14 @@ public class PersonDao {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
     }
+
+    public void update(int id, Person updatePerson) {
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updatePerson.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(person -> person.getId() == id);
+    }
 }
