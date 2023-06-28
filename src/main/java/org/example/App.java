c2bc63cdcb08cabf6915132a610058765d7afd33
Sergey Baklanov <sersbakalnov@gmail.com>
2023-06-28 21:58:04 +0300

    ls49 hibernate |  save in db

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.example.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            session.beginTransaction();
+
+            Person person1 = new Person("test1", 11);
+            Person person2 = new Person("test2", 12);
+            Person person3 = new Person("test3", 13);
+
+            session.save(person1);
+            session.save(person2);
+            session.save(person3);
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}
