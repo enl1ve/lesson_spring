d455b699f1ee6bb0e8091fac5d0a689ffd0235f6
Sergey Baklanov <sersbakalnov@gmail.com>
2023-07-11 22:40:31 +0300

    l71 first Spring Boot App

---
+++ src/test/java/ua/baklanov/spring_course/SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.baklanov.spring_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
